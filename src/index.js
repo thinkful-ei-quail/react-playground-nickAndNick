2267cc99bbb21be68eaef5c5f1c34d697889b665
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-07-22 09:01:11 -0700

    start work on Bomb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './state-drills/HelloWorld';
+import Bomb from './state-drills/Bomb';
 import './index.css';
 
-ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+ReactDOM.render(<Bomb />, document.getElementById('root'));
---
+++ src/state-drills/Bomb.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Bomb extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            const newCount = this.state.count + 1
+            this.setState({
+                count: newCount
+            })
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
+    }
+
+    
+    render() {
+        const bombBoom = this.count;
+        return (
+            <div>
+                <p>{bombBoom}</p>
+            </div>
+        )
+    }
+
+}
+
+export default Bomb;
