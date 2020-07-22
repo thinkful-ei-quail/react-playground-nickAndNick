02ebeca1d49033eb0be0de7fc6292888c849cd32
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-07-22 11:17:45 -0700

    handleTriggerClick work on RouletteGun

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './state-drills/HelloWorld';
-import Bomb from './state-drills/Bomb';
+// import HelloWorld from './state-drills/HelloWorld';
+// import Bomb from './state-drills/Bomb';
 import RouletteGun from './state-drills/RouletteGun';
 import './index.css';
 
-ReactDOM.render(<Bomb />, document.getElementById('root'));
+ReactDOM.render(<RouletteGun bulletInChamber={8} />, document.getElementById('root'));
--- src/state-drills/RouletteGun.js
+++ src/state-drills/RouletteGun.js
@@ -2,14 +2,41 @@ import React, { Component } from 'react';
 
 
 class RouletteGun extends Component {
-    render() {
-        return(
-        <div>
-            <p></p>
-        </div>)
+    constructor(props) {
+        super(props);
+        this.state = {
+            chamber: null,
+            spinningTheChamber: false
+        }
     }
-    
-    ;
+
+    static defaultProps = {
+        bulletInChamber: 8
+    }
+
+    handleTriggerClick = () => {
+        this.setState({
+            spinningTheChamber: true
+        })
+        let insideTimeout = 
+            this.setState({
+                chamber: Math.ceil(Math.random() * 8)
+            })
+            this.setState({
+                spinningTheChamber: false
+            })
+        let timeout = setTimeout(insideTimeout, 2000)
+        return timeout;
+    }
+
+    render() {
+        return (
+            <div>
+                <p>{this.state.chamber}</p>
+                <button onClick={this.handleTriggerClick}>Pull the trigger!</button>
+            </div>
+        );
+    };
 };
 
 export default RouletteGun;
